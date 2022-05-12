e5049a13defdf644d516841728bf4446008cc0dc
Merajhosen <merajakhon7@gmail.com>
2022-05-12 21:44:53 +0600

    navabr and heru section add

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
