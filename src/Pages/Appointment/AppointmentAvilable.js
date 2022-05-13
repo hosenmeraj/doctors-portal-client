3f302a08836859f6246113559669742f24338f31
Merajhosen <merajakhon7@gmail.com>
2022-05-13 15:36:53 +0600

    appointment booking appointment dynamic modal adding

--- src/Pages/Appointment/AppointmentAvilable.js
+++ src/Pages/Appointment/AppointmentAvilable.js
@@ -1,8 +1,11 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingAppointment from './BookingAppointment';
+import Service from './Service';
 
 const AppointmentAvilable = ({ date }) => {
     const [services, setServices] = useState([])
+    const [treatment, setTreatment] = useState(null)
     useEffect(() => {
         fetch('services.json')
             .then(res => res.json())
@@ -13,11 +16,16 @@ const AppointmentAvilable = ({ date }) => {
             <div>
                 <h4 className='text-xl text-center text-secondary'>Available Appoinment : {format(date, 'PP')}</h4>
             </div>
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(service => <li key={service._id}>{service.name}</li>)
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                        setTreatment={setTreatment}
+                    ></Service>)
                 }
             </div>
+            {treatment && <BookingAppointment treatment={treatment}></BookingAppointment>}
         </div>
     );
 };
